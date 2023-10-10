c476a7e29e67d97df30bbde698fca4d799d60231
Eric <ethai1@cps.edu>
2023-10-10 11:08:30 -0500

    rating, info questions added

--- a3.py
+++ a3.py
@@ -253,6 +253,10 @@ pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
     (str.split("who acted in %"), actors_by_title),
     (str.split("when was % made"), year_by_title),
     (str.split("in what movies did % appear"), title_by_actor),
+
+    (str.split("what is the rating of %"), rating_by_title),
+    (str.split("what is the info of %"), info_by_title),
+
     (["bye"], bye_action),
 ]
 
